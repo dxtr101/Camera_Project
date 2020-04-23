e51accbea07574254878d5fe7692c642c79958a3
dxtr101 <51464973+dxtr101@users.noreply.github.com>
2020-04-23 11:45:03 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,110 @@
+from __future__ import print_function
+from matplotlib import pyplot as plt
 import cv2 as cv
 import numpy as np
-import matplotlib as plt
+import argparse
+import time
 
-# Video Stream
-video = cv.VideoCapture(0)
-
-# Save File Information
-fourcc = cv.VideoWriter_fourcc(*'XVID')
-out = cv.VideoWriter('output.avi', fourcc, 20, (640, 480))
-
-if not video.isOpened():
-    print("Cannot open Camera")
-    exit()
-while video.isOpened():
-    # Frame by Frame capture
-    ret, frame = video.read()
+def bkg_frame(vid):
+    '''
+    This function obtains a frame from the video feed and saves it as the background image.
+    :param vid: video object
+    :return: none
+    '''
+    ret, frame = vid.read()
 
     if not ret:
-        print("Can't get frame. Exiting...")
-        break
+        print("Can't get background image.  Exiting...")
+        exit()
+    else:
+        print("Updated background image.")
 
-    # Processing
-    # video_grey = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imwrite('background.png', frame)
+    return time.time()
 
-    # Display Image
-    cv.imshow('Grey Video', frame)
-    if cv.waitKey(1) == ord('q'):
-        break
+def main():
+    # Video Stream
+    video = cv.VideoCapture(0)
 
-    # Write Frame to File
-    out.write(frame)
+    if not video.isOpened():
+        print("Cannot open Camera")
+        exit()
 
-# Release capture
-video.release()
-out.release()
-cv.destroyAllWindows()
+    # Configuration
+    plt.ion()
+    bkg_update_period = 6000 # seconds between background updates
+    motion_threshold = 370   # Min histogram value that indicates movement
+    motion = False           # Initially, no motion detected.
+    time.sleep(1)
+
+    # Get Background Frame
+    bkg_time = bkg_frame(video)
+
+    # Save File Information
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
+    out = cv.VideoWriter('output.avi', fourcc, 20, (640, 480))
+
+    # Parser (Detect Differences From Background)
+    parser = argparse.ArgumentParser(description='This program detections motion via background subtraction.')
+    parser.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default='background.png')
+    parser.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2).', default='MOG2')
+    args = parser.parse_args()
+
+    if args.algo == 'MOG2':
+        backSub = cv.createBackgroundSubtractorMOG2(500, 10.0, True)
+    else:
+        backSub = cv.createBackgroundSubtractorKNN(500, 400.0, True)
+
+    while video.isOpened():
+        loop_time = time.time()
+        # Frame by Frame capture
+        ret, frame = video.read()
+
+        if not ret:
+            print("Can't get frame. Exiting...")
+            break
+
+        # Processing
+        processed_frame = cv.GaussianBlur(frame, (5,5), 0)
+        fgMask = backSub.apply(processed_frame)
+        hist_mask = cv.calcHist([processed_frame], [0], fgMask, [256], [0, 256])
+
+        # Determine if Motion Detected
+        hist_mask_max = np.amax(hist_mask)
+        if hist_mask_max > motion_threshold:
+            motion = True
+        else:
+            motion = False
+
+        # Plot Histogram
+        # plt.clf()
+        # plt.axis([0, 256, 0, 5000])
+        # plt.plot(hist_mask)
+        # plt.draw()
+        # plt.pause(0.001)
+
+        # Display Image
+        cv.imshow('Video', frame)
+        cv.imshow('Mask', fgMask)
+        if cv.waitKey(1) == ord('q'):
+            break
+
+        # Update Background
+        if (loop_time - bkg_time) > bkg_update_period:
+            bkg_time = bkg_frame(video)
+
+        if motion:
+            # Add Time Stamp
+            cv.rectangle(frame, (10, 2), (240, 20), (255, 255, 255), -1)
+            cv.putText(frame, str(time.asctime()), (15, 15),
+                       cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+            # Write Frame to File
+            out.write(frame)
+
+    # Release capture
+    video.release()
+    out.release()
+    cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
